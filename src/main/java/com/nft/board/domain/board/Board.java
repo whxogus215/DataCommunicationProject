5b6b21ef2d0fdff0aa86bf50c944c73aefbaf563
whxogus215 <whxogus215@naver.com>
2023-10-05 16:44:06 +0900

    Feat : Board 엔티티 추가

---
+++ src/main/java/com/nft/board/domain/board/Board.java
@@ -0,0 +1,18 @@
+package com.nft.board.domain.board;
+
+import java.util.Date;
+
+public class Board {
+    private int boardId; // 게시글 ID
+    private String title; // 게시글 제목
+    private String content; // 게시글 내용
+
+    private int price; // 후원 금액
+
+    private Date createdDate; // 작성일
+    private Date modifiedDate; // 수정일
+
+    private int adminId; // 운영자(게시글 작성자) ID
+    private int imageId; // 첨부된 이미지 ID
+
+}
