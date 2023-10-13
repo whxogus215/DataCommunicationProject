0ae1659f606ab59b38848385a0b455c60c87318f
whxogus215 <whxogus215@naver.com>
2023-10-14 07:42:41 +0900

    Feat : Admin 저장 메서드 추가

--- src/main/java/com/nft/board/domain/user/admin/JdbcAdminRepository.java
+++ src/main/java/com/nft/board/domain/user/admin/JdbcAdminRepository.java
@@ -1,14 +1,11 @@
 package com.nft.board.domain.user.admin;
 
+import java.util.Optional;
+
 public interface JdbcAdminRepository {
 
     /*
-    1. admin_loginid 가져오기
-    2. admin_password 가져오기
-
-
-
-
-
+    1. ID로 Admin 엔티티 가져오기 (select + where) : Admin 객체 or null 반환
      */
+    Optional<Admin> findAdminById(String Id);
 }
