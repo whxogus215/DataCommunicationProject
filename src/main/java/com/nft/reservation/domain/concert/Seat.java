9b45da449d2cd785cfb41ca1bc5d9824813db3fa
whxogus215 <whxogus215@naver.com>
2023-10-31 22:42:34 +0900

    feat: column 필드 숫자 -> 문자 변경

--- src/main/java/com/nft/reservation/domain/concert/Seat.java
+++ src/main/java/com/nft/reservation/domain/concert/Seat.java
@@ -2,15 +2,17 @@ package com.nft.reservation.domain.concert;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Seat {
     // PK
     private Integer id;
 
     private Integer row;
-    private Integer column;
+    private String column;
     private boolean isBook;
 
     // FK
