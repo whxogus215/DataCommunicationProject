7308b677d1e53ca9e35ca767a643978b2a1464e7
whxogus215 <whxogus215@naver.com>
2023-10-31 22:42:59 +0900

    test: 특정 공연 좌석 조회 테스트 성공

--- src/test/java/com/nft/reservation/domain/concert/H2ConcertRepositoryTest.java
+++ src/test/java/com/nft/reservation/domain/concert/H2ConcertRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import com.nft.reservation.domain.board.H2BoardRepository;
 import com.zaxxer.hikari.HikariDataSource;
+import java.util.List;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
@@ -45,5 +46,16 @@ class H2ConcertRepositoryTest {
         assertThat(findConcert).isNotNull();
 
         log.info("조회한 Concert 출력 : {}", findConcert.get());
+    }
+
+    @Test
+    @DisplayName("특정 공연의 좌석 조회 메서드 테스트")
+    void findBookedSeatByIdTest() {
+        int testId = 1;
+
+        List<Seat> bookedSeatById = jdbcConcertRepository.findBookedSeatById(testId);
+
+        bookedSeatById.stream()
+                        .forEach((seat) -> log.info("조회한 Seat 출력 : {}", seat.toString()));
     }
 }
