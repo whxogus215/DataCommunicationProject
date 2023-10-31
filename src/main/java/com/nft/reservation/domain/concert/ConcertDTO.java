8874eaafcab08bb11500761e776c4d0beaa83f4a
whxogus215 <whxogus215@naver.com>
2023-10-31 15:32:00 +0900

    feat: ConcertDTO 구현

---
+++ src/main/java/com/nft/reservation/domain/concert/ConcertDTO.java
@@ -0,0 +1,15 @@
+package com.nft.reservation.domain.concert;
+
+import java.sql.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConcertDTO {
+    private Integer id;
+    private String title;
+    private Date date;
+    private Integer runningTime;
+    private String place;
+}
