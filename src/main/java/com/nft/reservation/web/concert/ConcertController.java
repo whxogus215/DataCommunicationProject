b5141e8a5df8d0c4ee1950240d9f5f5add7205b3
whxogus215 <whxogus215@naver.com>
2023-10-31 16:14:43 +0900

    feat: ConcertController Swagger UI 추가

--- src/main/java/com/nft/reservation/web/concert/ConcertController.java
+++ src/main/java/com/nft/reservation/web/concert/ConcertController.java
@@ -2,12 +2,14 @@ package com.nft.reservation.web.concert;
 
 import com.nft.reservation.domain.concert.ConcertDTO;
 import com.nft.reservation.domain.concert.ConcertService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "콘서트", description = "콘서트 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/concert")
