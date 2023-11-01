bf8682755697b8815ec3d65d63eda9fca383a856
whxogus215 <whxogus215@naver.com>
2023-11-01 14:12:14 +0900

    feat: ConcertHall -> ConcertHallDTO 매퍼 메서드 구현

--- src/main/java/com/nft/reservation/domain/mapper/ReservationMapper.java
+++ src/main/java/com/nft/reservation/domain/mapper/ReservationMapper.java
@@ -1,11 +1,15 @@
 package com.nft.reservation.domain.mapper;
 
 import com.nft.reservation.domain.concert.Concert;
+import com.nft.reservation.domain.concert.ConcertHall;
 import com.nft.reservation.domain.concert.Seat;
 import com.nft.reservation.web.concert.dto.ConcertDTO;
+import com.nft.reservation.web.concert.dto.ConcertHallDTO;
 import com.nft.reservation.web.concert.dto.SeatDTO;
 
 public interface ReservationMapper {
     ConcertDTO concertToConcertResponseDTO(Concert concert);
     SeatDTO seatToSeatResponseDTO(Seat seat);
+
+    ConcertHallDTO concertHallToConcertHallDTO(ConcertHall concertHall);
 }
--- src/main/java/com/nft/reservation/domain/mapper/ReservationMapperImpl.java
+++ src/main/java/com/nft/reservation/domain/mapper/ReservationMapperImpl.java
@@ -1,8 +1,10 @@
 package com.nft.reservation.domain.mapper;
 
 import com.nft.reservation.domain.concert.Concert;
+import com.nft.reservation.domain.concert.ConcertHall;
 import com.nft.reservation.domain.concert.Seat;
 import com.nft.reservation.web.concert.dto.ConcertDTO;
+import com.nft.reservation.web.concert.dto.ConcertHallDTO;
 import com.nft.reservation.web.concert.dto.SeatDTO;
 import org.springframework.stereotype.Component;
 
@@ -11,33 +13,47 @@ public class ReservationMapperImpl implements ReservationMapper {
 
     @Override
     public ConcertDTO concertToConcertResponseDTO(Concert concert) {
-        if ( concert == null ) {
+        if (concert == null) {
             return null;
         }
 
         ConcertDTO concertDTO = new ConcertDTO();
 
-        concertDTO.setId( concert.getId() );
-        concertDTO.setTitle( concert.getTitle() );
-        concertDTO.setDate( concert.getDate() );
-        concertDTO.setRunningTime( concert.getRunningTime() );
-        concertDTO.setPlace( concert.getPlace() );
+        concertDTO.setId(concert.getId());
+        concertDTO.setTitle(concert.getTitle());
+        concertDTO.setDate(concert.getDate());
+        concertDTO.setRunningTime(concert.getRunningTime());
+        concertDTO.setPlace(concert.getPlace());
 
         return concertDTO;
     }
 
     @Override
     public SeatDTO seatToSeatResponseDTO(Seat seat) {
-        if ( seat == null ) {
+        if (seat == null) {
             return null;
         }
 
         SeatDTO seatDTO = new SeatDTO();
 
-        seatDTO.setRow( seat.getRow() );
-        seatDTO.setCol( seat.getCol() );
-        seatDTO.setData( seat.isData() );
+        seatDTO.setRow(seat.getRow());
+        seatDTO.setCol(seat.getCol());
+        seatDTO.setData(seat.isData());
 
         return seatDTO;
+    }
+
+    @Override
+    public ConcertHallDTO concertHallToConcertHallDTO(ConcertHall concertHall) {
+        if (concertHall == null) {
+            return null;
+        }
+
+        ConcertHallDTO concertHallDTO = new ConcertHallDTO();
+
+        concertHallDTO.setRowSize(concertHall.getRowSize());
+        concertHallDTO.setColumnSize(concertHall.getColumnSize());
+
+        return concertHallDTO;
     }
 }
