749cdbfa632f406f46f936d36074515fa2b404ab
whxogus215 <whxogus215@naver.com>
2023-11-08 22:47:49 +0900

    feat: 이미지 Entity 추가

---
+++ src/main/java/com/nft/reservation/domain/concert/entity/Image.java
@@ -0,0 +1,19 @@
+package com.nft.reservation.domain.concert.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Image {
+
+    private String uploadName; // 업로드 이름
+    private String storeName; // UUID 이름
+
+    public Image(String uploadName, String storeName) {
+        this.uploadName = uploadName;
+        this.storeName = storeName;
+    }
+
+}
