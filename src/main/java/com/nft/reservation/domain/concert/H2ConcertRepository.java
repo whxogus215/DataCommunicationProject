4b227345b9ba03557b3e5dcb2004c9f5a9f3b705
whxogus215 <whxogus215@naver.com>
2023-11-12 15:17:00 +0900

    refactor(Repository): JdbcTemplate 적용

--- src/main/java/com/nft/reservation/domain/concert/H2ConcertRepository.java
+++ src/main/java/com/nft/reservation/domain/concert/H2ConcertRepository.java
@@ -13,6 +13,8 @@ import java.util.Optional;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.support.JdbcUtils;
 import org.springframework.stereotype.Repository;
@@ -22,71 +24,18 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class H2ConcertRepository implements JdbcConcertRepository{
 
-    private final DataSource dataSource;
+    private final JdbcTemplate template;
 
     @Override
     public Optional<Concert> findById(Integer id) {
         String sql = "select * from concert where id = ?";
-
-        Connection con = getConnection();
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, id);
-            rs = pstmt.executeQuery();
-            while (rs.next()) {
-                Concert concert = new Concert();
-
-                concert.setId(rs.getInt(1));
-                concert.setTitle(rs.getString(2));
-                concert.setDay(rs.getString(3));
-                concert.setRunningTime(4);
-                concert.setPlace(rs.getString(5));
-                concert.setRankId(rs.getInt(6));
-                concert.setHallId(rs.getInt(7));
-
-                return Optional.of(concert);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            close(con, pstmt, rs);
-        }
-        return Optional.empty();
+        return template.queryForObject(sql, concertRowMapper(), id);
     }
 
     @Override
     public List<Seat> findBookedSeatById(Integer id) {
         String sql = "select * from seat where id = ?";
-
-        Connection con = getConnection();
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, id);
-            rs = pstmt.executeQuery();
-
-            List<Seat> seats = new ArrayList<>();
-
-            while (rs.next()) {
-                Seat seat = new Seat();
-
-                seat.setRow(rs.getInt(2));
-                seat.setCol(rs.getString(3).charAt(0));
-                seat.setData(rs.getBoolean(4));
-
-                seats.add(seat);
-            }
-            return seats;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            close(con, pstmt, rs);
-        }
+        return template.queryForObject(sql, seatRowMapper(), id);
     }
 
     @Override
@@ -95,44 +44,52 @@ public class H2ConcertRepository implements JdbcConcertRepository{
                      "FROM concert AS c " +
                      "LEFT JOIN hall AS h ON c.hall_id = h.id " +
                      "WHERE c.id = ?";
-
-        Connection con = getConnection();
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, id);
-            rs = pstmt.executeQuery();
-            while (rs.next()) {
-                ConcertHall concertHall = new ConcertHall();
-
-                concertHall.setName(rs.getString(1));
-                concertHall.setCapacity(rs.getInt(2));
-                concertHall.setRowSize(rs.getInt(3));
-                concertHall.setColumnSize(rs.getInt(4));
-
-                return Optional.of(concertHall);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            close(con, pstmt, rs);
-        }
-        return Optional.empty();
+        return template.queryForObject(sql, concertHallRowMapper(), id);
     }
 
-    private Connection getConnection() {
-        return DataSourceUtils.getConnection(dataSource);
+    private RowMapper<Optional<Concert>> concertRowMapper() {
+        return (rs, rowNum) -> {
+            Concert concert = new Concert();
+
+            concert.setId(rs.getInt(1));
+            concert.setTitle(rs.getString(2));
+            concert.setDay(rs.getString(3));
+            concert.setRunningTime(4);
+            concert.setPlace(rs.getString(5));
+            concert.setRankId(rs.getInt(6));
+            concert.setHallId(rs.getInt(7));
+
+            return Optional.of(concert);
+        };
     }
 
-    private void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
-        JdbcUtils.closeResultSet(rs);
-        JdbcUtils.closeStatement(pstmt);
+    private RowMapper<List<Seat>> seatRowMapper() {
+        return (rs, rowNum) -> {
+            List<Seat> seats = new ArrayList<>();
 
-        // 트랜잭션 동기화를 위해 DataSourceUtils 메서드 사용
-        DataSourceUtils.releaseConnection(con, dataSource);
+            Seat seat = new Seat();
+
+            seat.setRow(rs.getInt(2));
+            seat.setCol(rs.getString(3).charAt(0));
+            seat.setData(rs.getBoolean(4));
+
+            seats.add(seat);
+
+            return seats;
+        };
     }
 
+    private RowMapper<Optional<ConcertHall>> concertHallRowMapper() {
+        return (rs, rowNum) -> {
+            ConcertHall concertHall = new ConcertHall();
+
+            concertHall.setName(rs.getString(1));
+            concertHall.setCapacity(rs.getInt(2));
+            concertHall.setRowSize(rs.getInt(3));
+            concertHall.setColumnSize(rs.getInt(4));
+
+            return Optional.of(concertHall);
+        };
+    }
 
 }
