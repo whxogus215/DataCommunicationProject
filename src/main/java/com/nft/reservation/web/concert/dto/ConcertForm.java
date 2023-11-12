34e5f4cbcdf1f708d29a4f96b1f6592829405aff
whxogus215 <whxogus215@naver.com>
2023-11-12 23:25:22 +0900

    refactor(ConcertForm): ConcertForm이 ConcertDTO를 포함하도록 리팩토링

--- src/main/java/com/nft/reservation/web/concert/dto/ConcertForm.java
+++ src/main/java/com/nft/reservation/web/concert/dto/ConcertForm.java
@@ -8,22 +8,13 @@ import org.springframework.web.multipart.MultipartFile;
 @Setter
 @Getter
 public class ConcertForm {
+    private ConcertDTO concertDTO;
 
-    private String concertTitle;
-    private String concertDay;
-    private int runningTime;
-    private String castMember;
-
-    private String hallName;
     private String hallCapacity;
     private String hallRow;
     private String hallColumn;
-    private String hallAddress;
-
-    private String rankDetail;
 
     private MultipartFile thumbnailImage;
     private MultipartFile carouselImage;
     private List<MultipartFile> contentImages;
-
 }
