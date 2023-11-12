01ccb17feaf9a8ac7e96bf8268a9365d7fbff283
whxogus215 <whxogus215@naver.com>
2023-11-12 23:02:18 +0900

    feat(ConcertDTO): 공연 DTO 업데이트

--- src/main/java/com/nft/reservation/web/concert/dto/ConcertDTO.java
+++ src/main/java/com/nft/reservation/web/concert/dto/ConcertDTO.java
@@ -1,15 +1,25 @@
 package com.nft.reservation.web.concert.dto;
 
-import java.sql.Date;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ConcertDTO {
-    private Integer id;
+    private Long id;
     private String title;
     private String day;
-    private Integer runningTime;
-    private String place;
+    private String rate; // 관람 등급
+    private Long runningTime;
+    private String castMember;
+
+    private String hallName; // 공연장 이름
+    private String hallAddress; // 공연장 주소
+
+    private String thumbnailUrl; // 썸네일 사진
+    private String carouselUrl; // 캐러셀 사진
+    private List<String> images; // 본문에 들어가는 소개 사진
 }
