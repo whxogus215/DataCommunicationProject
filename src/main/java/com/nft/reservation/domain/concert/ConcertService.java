5d097e09ea00e731bfe42c0ef0006507de7b6750
whxogus215 <whxogus215@naver.com>
2023-11-12 11:56:55 +0900

    feat(ConcertService): 공연등록 인터페이스 추가

--- src/main/java/com/nft/reservation/domain/concert/ConcertService.java
+++ src/main/java/com/nft/reservation/domain/concert/ConcertService.java
@@ -1,6 +1,7 @@
 package com.nft.reservation.domain.concert;
 
 import com.nft.reservation.web.concert.dto.ConcertDTO;
+import com.nft.reservation.web.concert.dto.ConcertForm;
 import com.nft.reservation.web.concert.dto.ConcertHallDTO;
 import com.nft.reservation.web.concert.dto.SeatDTO;
 import java.util.List;
@@ -8,6 +9,6 @@ import java.util.List;
 public interface ConcertService {
     ConcertDTO getConcertDetail(Integer concertId);
     List<SeatDTO> getConcertSeat(Integer concertId);
-
     ConcertHallDTO getConcertHallSize(Integer concertId);
+    void createConcert(ConcertForm concertForm);
 }
