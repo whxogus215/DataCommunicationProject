17a4d7c06325f239227a1467191fb6039a7c287a
whxogus215 <whxogus215@naver.com>
2023-11-14 22:10:30 +0900

    refactor(Image): 생성자 에너테이션 추가

--- src/main/java/com/nft/reservation/domain/concert/entity/Image.java
+++ src/main/java/com/nft/reservation/domain/concert/entity/Image.java
@@ -1,20 +1,19 @@
 package com.nft.reservation.domain.concert.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Image {
 
     private String uploadName; // 업로드 이름
     private String storeName; // UUID 이름
     private Long concertId; // 콘서트 ID (FK)
-
-    public Image(String uploadName, String storeName) {
-        this.uploadName = uploadName;
-        this.storeName = storeName;
-    }
 
 }
