d056e6e611d5b7f72a27f7a0383ec235d9056151
whxogus215 <whxogus215@naver.com>
2023-11-14 16:53:23 +0900

    feat(Image): 콘서트 ID 저장 필드 추가

--- src/main/java/com/nft/reservation/domain/concert/entity/Image.java
+++ src/main/java/com/nft/reservation/domain/concert/entity/Image.java
@@ -10,6 +10,7 @@ public class Image {
 
     private String uploadName; // 업로드 이름
     private String storeName; // UUID 이름
+    private Long concertId; // 콘서트 ID (FK)
 
     public Image(String uploadName, String storeName) {
         this.uploadName = uploadName;
