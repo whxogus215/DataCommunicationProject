af9dc9715ea2f1f44e1636af81df154d5b389566
whxogus215 <whxogus215@naver.com>
2023-11-15 13:21:42 +0900

    feat(Mapper): 공연장 주소 임시 데이터 저장

--- src/main/java/com/nft/reservation/domain/mapper/ReservationMapperImpl.java
+++ src/main/java/com/nft/reservation/domain/mapper/ReservationMapperImpl.java
@@ -24,7 +24,7 @@ public class ReservationMapperImpl implements ReservationMapper {
         concertDTO.setRunningTime(concert.getRunningTime());
 
         // 임시 데이터 추가
-        concertDTO.setHallAddress("서울 송파구 올림픽로 424");
+        concertDTO.setHallAddress("서울특별시 송파구 올림픽로 424");
 
         return concertDTO;
     }
