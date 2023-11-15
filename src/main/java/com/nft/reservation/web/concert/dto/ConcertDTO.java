e2f5773a7b2333c195beb24ec5299a7168fd6dde
whxogus215 <whxogus215@naver.com>
2023-11-15 12:02:06 +0900

    feat(ConcertDTO): 각 필드별로 상세 설명 추가

--- src/main/java/com/nft/reservation/web/concert/dto/ConcertDTO.java
+++ src/main/java/com/nft/reservation/web/concert/dto/ConcertDTO.java
@@ -1,5 +1,6 @@
 package com.nft.reservation.web.concert.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,17 +10,36 @@ import lombok.ToString;
 @Setter
 @ToString
 public class ConcertDTO {
+    @Schema(description = "공연 ID")
     private Long id;
+
+    @Schema(description = "공연 제목")
     private String title;
+
+    @Schema(description = "공연 날짜")
     private String day;
-    private String rate; // 관람 등급
+
+    @Schema(description = "관람 등급")
+    private String rate;
+
+    @Schema(description = "공연 시간")
     private Long runningTime;
+
+    @Schema(description = "출연진")
     private String castMember;
 
-    private String hallName; // 공연장 이름
-    private String hallAddress; // 공연장 주소
+    @Schema(description = "공연장 이름")
+    private String hallName;
 
-    private String thumbnailUrl; // 썸네일 사진
-    private String carouselUrl; // 캐러셀 사진
-    private List<String> images; // 본문에 들어가는 소개 사진
+    @Schema(description = "공연장 주소")
+    private String hallAddress;
+
+    @Schema(description = "썸네일 사진 URL")
+    private String thumbnailUrl;
+
+    @Schema(description = "캐러셀 사진 URL")
+    private String carouselUrl;
+
+    @Schema(description = "상세 설명에 들어가는 사진 URL 리스트")
+    private List<String> images;
 }
