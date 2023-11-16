e8f9d10923af3f303f724226e077a65f0565543d
whxogus215 <whxogus215@naver.com>
2023-11-16 16:25:40 +0900

    feat(ImageSorter): 썸네일 이미지 반환 메서드 구현

---
+++ src/main/java/com/nft/reservation/domain/image/ImageSorter.java
@@ -0,0 +1,35 @@
+package com.nft.reservation.domain.image;
+
+import com.nft.reservation.domain.concert.entity.Image;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImageSorter {
+    private static final String THUMB_NAIL = "썸네일";
+    private static final String CAROUSEL = "캐러셀";
+    private static final String CONTENT = "본문";
+
+    // 썸네일 이미지 반환
+    // 캐러셀 이미지 반환
+    // 본문 이미지 리스트로 반환
+    public static Image getThumbNail(List<Image> images) {
+        return images.stream()
+                .filter(image -> image.getUploadName().contains(THUMB_NAIL))
+                .findAny()
+                .orElse(null);
+    }
+
+    public static Image getCarousel(List<Image> images) {
+        return null;
+    }
+
+    public static List<Image> getContents(List<Image> images) {
+        return null;
+    }
+
+
+
+
+
+}
