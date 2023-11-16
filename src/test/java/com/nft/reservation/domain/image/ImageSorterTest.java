eb4ea0f709a6a1fbdf23036f3fcd5e21c1076f3e
whxogus215 <whxogus215@naver.com>
2023-11-16 17:05:39 +0900

    test(ImageSorter): 본문 이미지 리스트 반환 메서드 구현 테스트

--- src/test/java/com/nft/reservation/domain/image/ImageSorterTest.java
+++ src/test/java/com/nft/reservation/domain/image/ImageSorterTest.java
@@ -6,24 +6,33 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.nft.reservation.domain.concert.JdbcConcertRepository;
 import com.nft.reservation.domain.concert.entity.Image;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class ImageSorterTest {
     @Autowired
     JdbcConcertRepository repository;
 
+    Long findId;
+    List<Image> findImages;
+
+    @BeforeEach
+    void before() {
+        findId = 101L;
+        findImages = repository.findImageByConcertId(findId);
+    }
+
     @Test
     @DisplayName("이미지 업로드명에 썸네일이 들어간 이미지 반환 테스트")
     void getThumbNailTest() {
-        Long findId = 101L;
-
-        List<Image> findImages = repository.findImageByConcertId(findId);
-
         Image thumbNail = ImageSorter.getThumbNail(findImages);
         assertThat(thumbNail.getUploadName()).contains("썸네일");
     }
@@ -31,11 +40,17 @@ class ImageSorterTest {
     @Test
     @DisplayName("이미지 업로드명에 캐러셀이 들어간 이미지 반환 테스트")
     void getCarouselTest() {
-        Long findId = 101L;
-
-        List<Image> findImages = repository.findImageByConcertId(findId);
-
         Image carousel = ImageSorter.getCarousel(findImages);
         assertThat(carousel.getUploadName()).contains("캐러셀");
+    }
+
+    @Test
+    @DisplayName("이미지 업로드명에 본문이 들어간 이미지 리스트 반환 테스트")
+    @MethodSource("findImages")
+    void getContentsTest() {
+        List<Image> contents = ImageSorter.getContents(findImages);
+        for (Image content : contents) {
+            assertThat(content.getUploadName().contains("본문"));
+        }
     }
 }
