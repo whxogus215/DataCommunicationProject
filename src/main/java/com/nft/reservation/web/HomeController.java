e70b1c7fb7bf6039f214e11237d43d881fb8479d
whxogus215 <whxogus215@naver.com>
2023-11-23 16:11:56 +0900

    feat: 홈 컨트롤러 구현

---
+++ src/main/java/com/nft/reservation/web/HomeController.java
@@ -0,0 +1,12 @@
+package com.nft.reservation.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "서버 정상 실행";
+    }
+}
