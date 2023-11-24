663cc2a3cf01c5691bd8aa2a0a42411762e915b1
whxogus215 <whxogus215@naver.com>
2023-11-24 16:55:56 +0900

    feat(ImageController): 이미지 GET API 요청이 잘 들어왔는지 로그 출력

--- src/main/java/com/nft/reservation/web/image/ImageController.java
+++ src/main/java/com/nft/reservation/web/image/ImageController.java
@@ -3,6 +3,7 @@ package com.nft.reservation.web.image;
 import com.nft.reservation.domain.concert.service.ConcertService;
 import java.net.MalformedURLException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ImageController {
 
     private final ConcertService concertService;
@@ -18,6 +20,7 @@ public class ImageController {
     @GetMapping("/images/{filename}")
     @ResponseBody
     public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        log.info("/images/{filename}으로 GET 요청 받음 : 전달받은 파일이름={}",filename);
         return concertService.getDownloadImage(filename);
     }
 }
